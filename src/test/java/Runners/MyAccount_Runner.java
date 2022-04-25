3520a77028ea5f4805771b0342ddcfb80cbb2598
preyas <preyas.rana@zymr.com>
2022-04-25 11:56:53 +0530

    Initial commit

---
+++ src/test/java/Runners/MyAccount_Runner.java
@@ -0,0 +1,18 @@
+package Runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = { "src/test/resources/Features/myaccount" }, glue = { "" }, monochrome = true, plugin = { "pretty",
+		"html:target/Html-reports/cucumber-reports.html", "json:target/Json-reports/cucumber.json",
+		"junit:target/XML-reports/Cucumber.xml", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"timeline:test-output-thread/"}, 
+       tags = "@smoketest and not @regression")
+
+public class MyAccount_Runner {
+	
+     
+}
